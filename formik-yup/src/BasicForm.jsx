60dec742760163b3ae6a6e71767539b5fb0ed7fd
Mert Gökmen <mertgkmeen@gmail.com>
2022-09-13 00:33:53 +0300

    icons

--- formik-yup/src/BasicForm.jsx
+++ formik-yup/src/BasicForm.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import * as yup from "yup";
 import "./BasicForm.css";
 import { useFormik } from "formik";
+import { mySvg } from "./icon";
+import { FaBeer } from "react-icons/fa";
+import { AiFillMinusCircle } from "react-icons/ai";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { TiTick } from "react-icons/ti";
 export const BasicForm = () => {
-  const passwordRegex =
-    "^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*s).{6,24}$";
-
   const validator = yup.object().shape({
     username: yup.string().max(30).required("Username is required."),
     email: yup
@@ -14,9 +16,8 @@ export const BasicForm = () => {
       .required("Email is required."),
     password: yup
       .string()
-      .min(6)
-      .max(24)
-      .matches(passwordRegex, { message: "Please enter stronger password." })
+      .min(6, "Password must be longer than 6 letters.")
+      .max(24, "Password must be shorter than 24 letters.")
       .required("Password is required."),
     passwordConfirm: yup
       .string()
@@ -47,61 +48,195 @@ export const BasicForm = () => {
     on_submit,
   });
 
-  console.log(errors);
-
   return (
     <div className="bg-dark block">
-      <form onSubmit={handleSubmit} autoComplete="off">
-        <div className="container">
-          <br />
-          <h5 className="mt-4">Username</h5>
-          <input
-            type="text"
-            value={values.username}
-            onChange={handleChange}
-            name="username"
-            className={`form-control shadow-sm ${
-              errors.username ? "input-error" : null
-            }`}
-          />
-          {errors.username && <p className="text-danger">{errors.username}</p>}
-          <h5 className="mt-4">Email</h5>
-          <input
-            type="email"
-            value={values.email}
-            onChange={handleChange}
-            name="email"
-            className={`form-control shadow-sm ${
-              errors.email ? "input-error" : null
-            }`}
-          />
-          {errors.email && <p className="text-danger">{errors.email}</p>}
-          <h5 className="mt-4">Password</h5>
-          <input
-            type="password"
-            value={values.password}
-            onChange={handleChange}
-            name="password"
-            className={`form-control shadow-sm ${
-              errors.password ? "input-error" : null
-            }`}
-          />
-          {errors.password && <p className="text-danger">{errors.password}</p>}
-          <h5 className="mt-4">Confirm Password</h5>
-          <input
-            type="password"
-            value={values.passwordConfirm}
-            onChange={handleChange}
-            name="passwordConfirm"
-            className={`form-control shadow-sm ${
-              errors.passwordConfirm ? "input-error" : null
-            }`}
-          />
-          {errors.passwordConfirm && (
-            <p className="text-danger">{errors.passwordConfirm}</p>
-          )}
+      <div className="container-fluid">
+        <div className="row align-items-center justify-content-center">
+          <div className="col-md-6 col-lg-7">
+            <h2 className="mt-4">Register</h2>
+            <form onSubmit={handleSubmit} autoComplete="off">
+              <h6 className="mt-4">Username</h6>
+              <div className="form-item position-relative">
+                <input
+                  type="text"
+                  value={values.username}
+                  onChange={handleChange}
+                  name="username"
+                  onBlur={handleBlur}
+                  className={`form-control shadow-sm ${
+                    errors.username && touched.username ? "input-error" : null
+                  } ${
+                    !errors.username && touched.username
+                      ? "input-success"
+                      : null
+                  }`}
+                />
+                <AiFillMinusCircle
+                  color="red"
+                  size={22}
+                  className={`position-absolute end-0 top-0 mt-2 me-2 ${
+                    errors.username && touched.username ? "" : "d-none"
+                  }`}
+                />
+                <TiTick
+                  color="green"
+                  size={22}
+                  className={`position-absolute end-0 top-0 mt-2 me-2 ${
+                    !errors.username && touched.username ? "" : "d-none"
+                  }`}
+                />
+              </div>
+
+              <p
+                className={`text-danger ${
+                  errors.username && touched.username ? "" : "opacity-0"
+                }`}
+              >
+                {errors.username && touched.username
+                  ? errors.username
+                  : "11111111111"}
+              </p>
+
+              <h6 className="mt-4">Email</h6>
+              <div className="form-item position-relative">
+                <input
+                  type="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  name="email"
+                  className={`form-control shadow-sm ${
+                    errors.email && touched.email ? "input-error" : null
+                  } ${!errors.email && touched.email ? "input-success" : null}`}
+                />
+                <AiFillMinusCircle
+                  color="red"
+                  size={22}
+                  className={`position-absolute end-0 top-0 mt-2 me-2 ${
+                    errors.email && touched.email ? "" : "d-none"
+                  }`}
+                />
+                <TiTick
+                  color="green"
+                  size={22}
+                  className={`position-absolute end-0 top-0 mt-2 me-2 ${
+                    !errors.email && touched.email ? "" : "d-none"
+                  }`}
+                />
+              </div>
+              <p
+                className={`text-danger ${
+                  errors.email && touched.email ? "" : "opacity-0"
+                }`}
+              >
+                {errors.email && touched.email ? errors.email : "11111111111"}
+              </p>
+
+              <h6 className="mt-4">Password</h6>
+              <div className="form-item position-relative">
+                <input
+                  type="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  name="password"
+                  className={`form-control shadow-sm ${
+                    errors.password && touched.password ? "input-error" : null
+                  } ${
+                    !errors.password && touched.password
+                      ? "input-success"
+                      : null
+                  }`}
+                />
+                <AiFillMinusCircle
+                  color="red"
+                  size={22}
+                  className={`position-absolute end-0 top-0 mt-2 me-2 ${
+                    errors.password && touched.password ? "" : "d-none"
+                  }`}
+                />
+                <TiTick
+                  color="green"
+                  size={22}
+                  className={`position-absolute end-0 top-0 mt-2 me-2 ${
+                    !errors.password && touched.password ? "" : "d-none"
+                  }`}
+                />
+              </div>
+              <p
+                className={`text-danger ${
+                  errors.password && touched.password ? "" : "opacity-0"
+                }`}
+              >
+                {errors.password && touched.password
+                  ? errors.password
+                  : "11111111111"}
+              </p>
+
+              <h6 className="mt-4">Confirm Password</h6>
+              <div className="form-item position-relative">
+                <input
+                  type="password"
+                  value={values.passwordConfirm}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  name="passwordConfirm"
+                  className={`form-control shadow-sm ${
+                    errors.passwordConfirm && touched.passwordConfirm
+                      ? "input-error"
+                      : null
+                  } ${
+                    !errors.passwordConfirm && touched.passwordConfirm
+                      ? "input-success"
+                      : null
+                  }`}
+                />
+                <AiFillMinusCircle
+                  color="red"
+                  size={22}
+                  className={`position-absolute end-0 top-0 mt-2 me-2 ${
+                    errors.passwordConfirm && touched.passwordConfirm
+                      ? ""
+                      : "d-none"
+                  }`}
+                />
+                <TiTick
+                  color="green"
+                  size={22}
+                  className={`position-absolute end-0 top-0 mt-2 me-2 ${
+                    !errors.passwordConfirm && touched.passwordConfirm
+                      ? ""
+                      : "d-none"
+                  }`}
+                />
+              </div>
+              <p
+                className={`text-danger ${
+                  errors.passwordConfirm && touched.passwordConfirm
+                    ? ""
+                    : "opacity-0"
+                }`}
+              >
+                {errors.passwordConfirm && touched.passwordConfirm
+                  ? errors.passwordConfirm
+                  : "11111111111"}
+              </p>
+
+              <div className="text-center">
+                <input
+                  type="submit"
+                  value="Register"
+                  className="submit-button mt-3"
+                />
+              </div>
+            </form>
+          </div>
+          <div className="col-md-6 col-lg-4 d-none d-md-inline-block">
+            <div>sadpajd</div>
+          </div>
         </div>
-      </form>
+        <br />
+      </div>
     </div>
   );
 };
